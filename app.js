779eae783f880f29a7ed52467b91a86c43000e56
Anna Jang <aj3spencer@gmail.com>
2026-09-16 18:10:40 -0700

    local hosting

---
+++ app.js
@@ -0,0 +1,15 @@
+require('dotenv').config();
+const express = require('express');
+const path = require('path');
+const port = process.env.PORT || 3000;
+const app = express();
+app.use(express.static(__dirname));
+
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, 'index.html'));
+});
+
+app.listen(port, () => {
+    console.log(`Server started on port ${port}`);
+});
